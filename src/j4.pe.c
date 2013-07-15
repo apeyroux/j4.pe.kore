5000791379645b7fb09a2fde8daa29f59e52c3bb
Jean-Alexandre Peyroux <m@j4.pe>
2013-07-15 23:20:27 +0200

    add /ip for set uri = j4.pe/ip in uzbl config

--- src/j4.pe.c
+++ src/j4.pe.c
@@ -4,10 +4,10 @@
 #include "static.h"
 
 int		serve_index(struct http_request *);
+int 	serve_ip(struct http_request *req);
 int		serve_qr(struct http_request *);
 
-int serve_index(struct http_request *req)
-{
+int serve_index(struct http_request *req) {
 	int ret;
 
 	http_response_header_add(req, "content-type", "text/html");
@@ -16,8 +16,22 @@ int serve_index(struct http_request *req)
 	return (ret);
 }
 
-int serve_qr(struct http_request *req)
-{
+int serve_ip(struct http_request *req) {
+	int ret;
+	char *json = NULL;
+
+	http_response_header_add(req, "content-type", "application/json");
+	struct in_addr addr = req->owner->sin.sin_addr;
+	char *ip = inet_ntoa(addr);
+	size_t jsonsize = strlen(ip) + strlen(req->agent) + strlen("{ip:,agent:}") + 1;
+	json = (char *) malloc(sizeof(char) * jsonsize);
+	snprintf(json, jsonsize, "{ip:%s,agent:%s}", ip, req->agent);
+	ret = http_response(req, 200, (u_int8_t *)json, jsonsize -1 );
+
+	return (ret);
+}
+
+int serve_qr(struct http_request *req) {
 	int		ret;
 
 	http_response_header_add(req, "content-type", "image/jpg");
