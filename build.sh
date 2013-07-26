c1455363e10ac2d623da269fb13ac75c1d0ea7ce
Jean-Alexandre Peyroux <m@j4.pe>
2013-07-26 19:37:13 +0200

    add install pandoc on openbsd 5.3 article

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ MEDIA_DIR=media
 SOURCE_DIR=src
 
 # The directory containing the Kore source code.
-KORE_DIR=../kore/kore
+KORE_DIR=/var/kore/kore
 if [ ${KORE_DIR} == "notset" ]; then
 	echo "Please edit build.sh and set KORE_DIR properly";
 	exit;
